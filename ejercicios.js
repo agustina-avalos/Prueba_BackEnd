22ebbf3c5ef21ab8cf946f89a267f114810c4489
Agustina <aguavalos@hotmail.com>
2023-10-01 18:57:26 -0300

    terminado

--- ejercicios.js
+++ ejercicios.js
@@ -13,26 +13,5 @@ function CaracterUnico(str) {
     return true; 
   }
   
-  console.log(hasUniqueCharacters("String")); // true
-  console.log(hasUniqueCharacters("Some String")); // false
-
-
-
-///ejercicio 2
-function FactoresPrimos(n) {
-    const factores = [];
-    let divisor = 2;
-  
-    while (n >= 2) {
-      if (n % divisor === 0) {
-        factores.push(divisor);
-        n = n / divisor;
-      } else {
-        divisor++;
-      }
-    }
-  
-    return factores;
-  }
-
-  
+  console.log(hasUniqueCharacters("String")); 
+  console.log(hasUniqueCharacters("Some String")); 
