941d9ef0a5038c307315981543b32b5dae7ea2a8
Agustina <aguavalos@hotmail.com>
2023-10-01 17:46:19 -0300

    coneccion db

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import { createPool } from "mysql2/promise";
+
+export const db = createPool({
+    host: 'precios-1.c0f6dm2ucnlg.us-east-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'candidatoPrueba',
+    password: 'gaspre21.M',
+    database: 'prueba'
+  });
+
+
+
+  db.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos', err);
+      return;
+    }
+    console.log('Conectado a la base de datos MySQL');
+  });
